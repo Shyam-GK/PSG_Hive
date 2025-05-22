4d67dbf0c82573ba7f74ed21b9fdc6bfa57c70e2
Shyam-GK <shyamgokulkrish@gmail.com>
2025-05-22 15:33:49 +0530

    Working in local without student profile and changed redis

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,6 @@
-// src/api.js
 const API_BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "process.env.REACT_APP_API_URL" // Replace with your production backend URL
+    ? process.env.REACT_APP_API_URL
     : "http://localhost:5000";
 
 export default API_BASE_URL;
